7fd1f8cf1236e233c39dd78acc59ccbccc34befe
Mujtaba Baig <baigmujtaba123@gmail.com>
2025-05-14 16:54:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Home from "./components/Home";
+const App = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4 ">
+            <div className="text-center mb-8">
+                <h1 className="text-5xl font-bold text-gray-800 mb-2">
+                    AI Image Enhancer{" "}
+                </h1>
+                <p className="text-lg text-gray-500">
+                    Upload your Image and let AI enhance to in seconds!
+                </p>
+            </div>
+
+            <Home />
+
+            <div className="mt-8 text-center">
+                <p className="text-sm text-gray-500">
+                    &copy; 2023 AI Image Enhancer. All rights reserved.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
